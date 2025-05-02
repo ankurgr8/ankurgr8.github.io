198d61d49e74cf7545e1fef07396d4e365bbb33d
ankurgr8 <ankursood0101@outlook.com>
2025-05-02 21:52:45 +0530

    yo

---
+++ experiment.script.js
@@ -0,0 +1,11 @@
+/*for (var i = 1; i <=50; i++) {
+    var num= 2*i ;
+    console.log("even number " + i + " is " + num);
+}*/
+// var a ="Experiment";
+// console.log(a.length);
+// // template literals
+// var block = `yoyo this ${3/3}s template literal`;
+// console.log(block);
+const name = prompt("Enter Your fullname: ");
+alert("@" + name + name.length);
